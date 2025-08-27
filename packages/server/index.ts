bbc597c7a22cd9249dcf7cb435309adefe94e00b
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-08-27 07:02:03 +0530

    added input validation using zod

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,41 +1,8 @@
-// import express from "express";
-// import type { Request, Response } from "express";
-// import dotenv from "dotenv";
-// import { GoogleGenAI } from "@google/genai";
-// dotenv.config();
-
-// const client = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
-// const app = express();
-// app.use(express.json());
-// const port = process.env.PORT || 3000;
-
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("hey");
-// });
-
-// app.get("/api/hello", (req: Request, res: Response) => {
-//   res.json({ message: "HEllo" });
-// });
-
-// app.post("/api/chat", async (req: Request, res: Response) => {
-//   const { prompt } = req.body;
-//   //send to gemini
-//   const response = await client.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents: prompt,
-//   });
-//   res.json({ message: response.text });
-// });
-
-// app.listen(port, () => {
-//   console.log("sever running fine");
-// });
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+import z from "zod";
 dotenv.config();
 
 const client = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
@@ -52,8 +19,21 @@ app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello" });
 });
 
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .min(1, "Prompt is required")
+    .max(1000, "Prompt is too long max 1000 characters"),
+});
+
 app.post("/api/chat", async (req: Request, res: Response) => {
   try {
+    const parseResult = chatSchema.safeParse(req.body);
+    if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+    }
+
     const { prompt } = req.body;
 
     const model = client.getGenerativeModel({ model: "gemini-2.0-flash" });
